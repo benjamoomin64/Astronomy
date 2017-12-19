e2f0f7f8d821d03b41c2a81aae764ce7c91f603f
Ben Anderson <anderson.bp13@gmail.com>
2017-12-18 17:40:13 -0800

    Created a new Project, with the intent of this one having a more complicated menu but a simplified backend.

---
+++ Astronomy2.0/Astronomy2.0/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Astronomy2._0
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("This should be a different thing");
+            Console.ReadLine();
+        }
+    }
+}
